6735325e30f11e5e61bb08fd9dd63b0a99ea1390
Francis <francispham89@gmail.com>
2018-09-24 17:27:44 -0700

    Wire Up Redux Thunk Middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import './index.css';
 
@@ -13,6 +14,12 @@ import Signup from './components/auth/Signup';
 
 import registerServiceWorker from './registerServiceWorker';
 
+
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
   <Provider store={createStore(reducers, {})}>
